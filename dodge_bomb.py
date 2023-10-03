b1090f838bd8c646854544b6b2e82a9ee8966985
Kudo <c0b22053f1@edu.teu.ac.jp>
2023-10-03 16:42:07 +0900

    演習２爆弾_途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,13 +5,50 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = {  # 練習３：移動量辞書
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+
+
+
+def check_bound(obj_rct: pg.Rect):
+    """
+    引数：こうかとんRectかばくだんRect
+    戻り値：タプル（横方向判定結果，縦方向判定結果）
+    画面内ならTrue，画面外ならFalse
+    """
+    yoko,tate = True,True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko,tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img2 = pg.transform.flip(kk_img,True,True)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900,400) #練習3　こうかとんの初期座標を設定
+    
+    kk_tr = { #角度を変えたこうかとん
+        (-5,0):pg.transform.rotozoom(kk_img, 0, 2.0),
+        (0,-5):pg.transform.rotozoom(kk_img,90,2.0),
+        (+5,0):pg.transform.rotozoom(kk_img2,0,2.0),
+        (+5,+5):pg.transform.rotozoom(kk_img2,-45,2.0),
+        (0,+5):pg.transform.rotozoom(kk_img2,-90,2.0),
+        (-5,-5):pg.transform.rotozoom(kk_img,-45,2.0),
+    }   
+    """ばくだん"""
+    
+    size_x,size_y = 0,0 
     bd_img = pg.Surface((20, 20))  # 練習１：爆弾Surfaceを作成する
     bd_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
@@ -26,11 +63,41 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
+        
+        
+        if kk_rct.colliderect(bd_rct): #練習5ぶつかっていたら
+            print("ゲームオーバー")
+            return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        
+        """こうかとん"""
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0,0]
+        for key,mv in delta.items(): #key:数字 mv:delta
+            if key_lst[key]:
+                sum_mv[0] += mv[0] # 練習３：横方向の合計移動量
+                sum_mv[1] += mv[1] # 練習３：縦方向の合計移動量
+        kk_rct.move_ip(sum_mv[0],sum_mv[1]) #練習3 移動させる
+        if check_bound(kk_rct) != (True,True): #練習4はみ出し判定
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
+        screen.blit(kk_img, kk_rct) #練習３ 移動後の座標に表示させる
+        
+        
+        """ばくだん"""
+        pg.transform.rotozoom(kk_img, 0, 2.0)
         bd_rct.move_ip(vx,vy) #練習2 爆弾を移動させる
-        screen.blit(bd_img, bd_rct)  # 練習１：Rectを使って試しにblit
+        yoko,tate = check_bound(bd_rct)
+        if not yoko: #練習４横方向にはみ出ていたら
+            vx *= -1
+        if not tate: #練習４横方向にはみ出たら
+            vy *= -1
+        if size_x and size_y < 20:
+            size_x += tmr
+            size_y += tmr
+        screen.blit(bd_img, bd_rct)  # 練習１ Rectを使って試しにblit
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
